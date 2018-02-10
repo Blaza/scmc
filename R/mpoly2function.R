27df65b996b1dd98bc6a643562a3d5a3a9b24f19
Blaza <blagoje.r.ivanovic@gmail.com>
2018-02-10 21:43:14 +0100

    Fix errors in lagrange and mpoly2function
    # Conflicts:
    #	R/lagrange.R
    #	R/mpoly2function.R

--- R/mpoly2function.R
+++ R/mpoly2function.R
@@ -76,7 +76,7 @@ mpoly2function <- function(x, varorder = vars(x), compile_c = FALSE){
   #### Generating function from mpoly_string and adding precomputing code for new vars
   ##### R version
   if (!compile_c) {
-    fun_args <- paste(varorder, collapse = ", ")
+    fun_args <- paste(varnames, collapse = ", ")
 
     fun_string <- glue("
       function({fun_args}) {{
@@ -94,13 +94,13 @@ mpoly2function <- function(x, varorder = vars(x), compile_c = FALSE){
     poly_string <- gsub(paste0(" ", var, " "), paste0(" ", var, "[i] "), poly_string)
   }
 
-  fun_args <- paste(sapply(varorder, function(var){
+  fun_args <- paste(sapply(varnames, function(var){
     paste0("NumericVector ", var)
   }), collapse = ", ")
 
   fun_string <- glue("
     NumericVector fun({fun_args}) {{
-      int n = {varorder[1]}.size();
+      int n = {varnames[1]}.size();
       NumericVector result(n);
       for (int i = 0; i < n; i++) {{
         result[i] = {poly_string};
